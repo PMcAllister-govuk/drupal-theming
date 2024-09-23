87fc2e7b56548c85ca88235afe2ecbf28cf3584e
PMcAllister-govuk <philip.mcallister@ofqual.gov.uk>
2024-09-23 16:51:03 +0100

    Setting up theme libraries

--- web/themes/ICDS/js/main.js
+++ web/themes/ICDS/js/main.js
@@ -1,5 +1,6 @@
-//src/index.js
+//js/main.js
 import "@ukic/fonts/dist/fonts.css";
 import "@ukic/web-components/dist/core/core.css";
+
 import { defineCustomElements } from "@ukic/web-components/loader";
 defineCustomElements();
